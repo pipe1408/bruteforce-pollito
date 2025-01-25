584834af8e9b9185fbaf5b9db8154fd448022424
sergi <sergio_socha@yahoo.com>
2025-01-25 15:03:54 -0500

    Agrega calculo de tiempo y agrega decifradas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,38 +2,63 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+
+        long tiempoInicio = System.nanoTime();
+
         StringBuilder texto;
         ArrayList<String> sospechasConfirmadas = new ArrayList<>();
         ArrayList<String> clavesDescifradas = new ArrayList<>();
+        ArrayList<Integer> posicionClaves = new ArrayList<>();
+
         String hash = "";
+
+
 
         for (int posicion = 0; posicion < DatosPollito.ROCKYOU_3000.length; posicion++) {
             texto = new StringBuilder(DatosPollito.ROCKYOU_3000[posicion]);
+            String textoOriginal = texto.toString();
+            int posicionHash = 0;
+
 
             for (String sospecha : DatosPollito.PALABRAS_COMUNES) {
-                if (texto.toString().equals(sospecha)) {
+                if (texto.toString().contains(sospecha)) {
                     sospechasConfirmadas.add(sospecha);
+                    posicionClaves.add(posicion);
                 }
             }
 
+
             for (int year = 1995; year <= 2024; year++) {
+                texto = new StringBuilder(textoOriginal);
                 texto.append(year).append("*");
+
                 try {
-                    hash = HashConverter.hash(String.valueOf(texto));
+                    hash = HashConverter.hash(texto.toString());
                 } catch (Exception e) {
-                    System.out.println("ERROR: " + e.getMessage());
+                    System.out.println("ERROR al convertir a hash la contraseña: " + texto + ". Mensaje: " + e.getMessage());
                 }
+
 
                 for (String clave : DatosPollito.CLAVES_EXPUESTAS) {
                     if (hash.equals(clave)) {
                         clavesDescifradas.add(texto.toString());
                     }
                 }
-                System.out.println(year);
             }
         }
 
-        System.out.println(sospechasConfirmadas);
-        System.out.println(clavesDescifradas);
+
+        long tiempoFin = System.nanoTime();
+
+        long tiempoTotal = tiempoFin - tiempoInicio;
+
+        double tiempoEnSegundos = tiempoTotal / 1_000_000_000.0;
+
+
+        System.out.println("Sospechas confirmadas: " + sospechasConfirmadas);
+        System.out.println("Claves descifradas: " + clavesDescifradas);
+        System.out.println("Posiciones sospechosos: " + posicionClaves);
+        System.out.println("El tiempo total de ejecución fue de: " + tiempoEnSegundos + " segundos");
+
     }
 }
